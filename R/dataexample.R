1f30875d940c7ca957903b0a7a3e8cf36005667c
ChubingZeng <chubingzeng@gmail.com>
2018-01-23 17:13:30 -0800

    Add all files

---
+++ R/dataexample.R
@@ -0,0 +1,8 @@
+#' @title some random data to use for the example
+#' @description Just a data
+#' @name x_data
+#' @docType data
+#' @usage x_data
+#' @format A matrix of random normal data
+#' @keywords datasets
+NULL
