7613da8458f520eb6ba39c00d01e4caf4a56f26b
Chubing Zeng <chubingzeng@gmail.com>
2019-04-15 23:13:41 -0700

    remove method = "lasso"

--- tests/testthat/test-userinput.R
+++ tests/testthat/test-userinput.R
@@ -11,14 +11,14 @@ test_that("throw error when X or Z do not have at least 2 columns", {
         X <- matrix(runif(10), ncol = 1)
         Y <- 1:10
         Z <- matrix(runif(10), 1)
-        expect_error(ipreg(X,Y,Z,method = "lasso"), "X must be a matrix with 2 or more columns")
+        expect_error(ipreg(X,Y,Z), "X must be a matrix with 2 or more columns")
 })
 
 test_that("throw error when dimensions of X and Y do not match", {
         X <- matrix(runif(100), nrow = 20)
         Y <- 1:10
         Z <- matrix(runif(10), nrow = 5)
-        expect_error(ipreg(X,Y,Z,method = "lasso"), paste("number of observations in Y (", length(Y), ") not equal to the number of rows of X (",
+        expect_error(ipreg(X,Y,Z), paste("number of observations in Y (", length(Y), ") not equal to the number of rows of X (",
                                                          nrow(X), ")", sep = ""),fixed = TRUE)
 })
 
@@ -26,7 +26,7 @@ test_that("throw error when ncol(X) not equal to nrow(Z)", {
         X <- matrix(runif(10), ncol = 2)
         Y <- 1:5
         Z <- matrix(runif(10), nrow = 5)
-        expect_error(ipreg(X,Y,Z,method = "lasso"), paste("number of rows in Z (", nrow(Z),
+        expect_error(ipreg(X,Y,Z), paste("number of rows in Z (", nrow(Z),
                                                           ") not equal to the number of columns in X (", ncol(X),
                                                           ")", sep = ""), fixed = TRUE)
 })
@@ -36,22 +36,29 @@ test_that("throw error when Z matrix contains non-numeric value",{
         Y <- 1:5
         Z <- matrix(runif(10), nrow = 2)
         Z[1,1] <- "0.88"
-        expect_error(ipreg(X,Y,Z,method = "lasso"), paste("Z contains non-numeric values"))
+        expect_error(ipreg(X,Y,Z), paste("Z contains non-numeric values"))
 })
 
 test_that("throw error when invalid estimated sigma square is provided",{
         X <- matrix(runif(10), ncol = 2)
         Y <- 1:5
         Z <- matrix(runif(10), nrow = 2)
-        expect_error(ipreg(X,Y,Z,sigma.square = -1,method = "lasso"), paste("sigma square should be a positive finite number"))
+        expect_error(ipreg(X,Y,Z,sigma.square = -1), paste("sigma square should be a positive finite number"))
 })
 
 test_that("throw error when invalid estimated sigma square is provided",{
         X <- matrix(runif(10), nrow = 5)
         Y <- 1:5
         Z <- matrix(runif(10), nrow = 2)
-        expect_error(ipreg(X,Y,Z,sigma.square = -1,method = "lasso"), paste("sigma square should be a positive finite number"))
+        expect_error(ipreg(X,Y,Z,sigma.square = -1), paste("sigma square should be a positive finite number"))
 })
 
-
+test_that("throw error when invalid estimated sigma square is provided",{
+        X <- matrix(runif(100), nrow = 20)
+        Y <- 1:20
+        Z <- matrix(runif(10), nrow = 5)
+        newX <- matrix(runif(100), nrow = 10)
+        ipreg_fitted <- ipreg(X,Y,Z)
+        expect_error(predict.ipreg(ipreg_fitted,newX),paste("New X does not have the same number of columns as X train"))
+})
 
