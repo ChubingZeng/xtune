8d9904e054c7d05679bcf26807568e431c4d5610
Chubing Zeng <chubingzeng@gmail.com>
2019-04-18 15:22:44 -0700

    objects documentation done

--- R/mse.R
+++ R/mse.R
@@ -1,14 +1,19 @@
 #' Calculate mean square error
 #'
 #' \code{mse} calculate mean square error (MSE) between prediction values and true values
-#' @param pred Prediction values
-#' @param true Actual values
+#' @param pred Prediction values vector
+#' @param true Actual values vector
+#' @return mean square error
+#' @examples
+#' Y1 <- rnorm(10,0,1)
+#' Y2 <- rnorm(10,0,1)
+#' mse(Y1,Y2)
 #' @export
 
 mse <- function(pred, true) {
     n_pred = ifelse(is.null(dim(pred)), length(pred), dim(pred)[1])
     n_true = ifelse(is.null(dim(true)), length(true), dim(true)[1])
-    
+
     if (length(pred) != length(true)) {
         stop("The length of prediction values and actual values does not match")
     } else if (sum(is.na(pred)) > 0) {
@@ -16,6 +21,6 @@ mse <- function(pred, true) {
     } else if (sum(is.na(true)) > 0) {
         warning("Actual values contain missing value")
     }
-    
+
     return(mean((as.vector(pred) - as.vector(true))^2, na.rm = T))
 }
