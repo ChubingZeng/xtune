fb951b1370d4e0535daec0c2b2b21a77e2e7a36b
Chubing Zeng <chubingzeng@gmail.com>
2019-05-09 17:36:21 -0700

    corret error

--- R/xtune.R
+++ R/xtune.R
@@ -82,7 +82,7 @@ xtune <- function(X, Y, Z = NULL,family=c("linear","binary"), sigma.square = NUL
                         stop("Y is not 0/1 binary variable")
                 }
                 family = "binary"
-        } else if (sort(unique(Y)) %in% c(0,1)){
+        } else if (sort(unique(Y)) == c(0,1)){
                 family = "binary"
         }
 
